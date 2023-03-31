0b706fae913847e49244cbdd326e7d1893303afa
mohitGitHxb <mohit2003second@gmail.com>
2023-03-31 17:53:42 +0530

    Doomed project: ViteReact/material-ui-project Axios is trash

---
+++ ViteReact/material-ui-project/src/components/Videos.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Videos = ({videos}) => {
+    console.log(videos);
+  return (
+    <div>
+      console.log(videos);
+    </div>
+  )
+}
+
+export default Videos
